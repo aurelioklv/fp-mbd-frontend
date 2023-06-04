beed75d10e43c8c2bbe786974c9166b21b529bf8
Aurelio Killian Lexi Verrill <aurelioklv@gmail.com>
2023-06-04 22:19:22 +0700

    feat: add home.js

---
+++ js/home.js
@@ -0,0 +1,81 @@
+fetch("http://localhost:5000/invoices")
+  .then((response) => response.json())
+  .then((invoices) => {
+    // Handle the retrieved invoice
+    // Select the elements
+    const container = document.querySelector(".li_container");
+    const dateOptions = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    };
+
+    const formatter = new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: "IDR",
+    });
+
+    invoices.forEach((invoice) => {
+      console.log(invoice);
+      console.log(invoice.li_id);
+      const date = new Date(invoice.li_due_date).toLocaleDateString(
+        "id-ID",
+        dateOptions
+      );
+      const money = formatter.format(invoice.li_total_payment);
+
+      console.log(invoice.li_total_payment);
+      console.log(money);
+
+      const invoiceElement = document.createElement("div");
+      invoiceElement.id = invoice.li_id;
+      invoiceElement.innerHTML = `
+        <div class="grid grid-cols-13 gap-5 justify-items-start">
+          <div>
+            <p class="text-sm text-slate-400 li_id">${invoice.li_id}</p>
+            <h2 class="text-lg font-semibold text-slate-700 li_total_payment">
+              ${money}
+            </h2>
+          </div>
+          <div class="col-start-5">
+            <p class="text-sm text-slate-400">Tenggat Pembayaran</p>
+            <h2 class="text-base font-semibold text-slate-700 li_due_date">
+              ${date}
+            </h2>
+          </div>
+          <div class="my-auto col-start-9">
+            <h2
+              class="text-sm font-semibold text-emerald-600 bg-emerald-50 py-1 px-4 w-auto rounded-full text-center border border-emerald-300"
+            >
+              Sudah dibayar
+            </h2>
+          </div>
+          <button
+            class="col-start-13 font-inter bg-slate-100 text-slate-500 font-medium w-auto py-2 px-8 rounded-md disabled:opacity-50"
+            disabled
+          >
+            Bayar
+          </button>
+        </div>`;
+      container.appendChild(invoiceElement);
+      // container.appendChild("<hr />");
+
+      console.log(invoice.li_status);
+      // const idElement = document.querySelector(".li_id");
+      // const totalPaymentElement = document.querySelector(".li_total_payment");
+      // const dueDateElement = document.querySelector(".li_due_date");
+      // // Insert the data into the elements
+      // idElement.innerHTML = invoice.li_id;
+      // totalPaymentElement.innerHTML = invoice.li_total_payment;
+      // dueDateElement.innerHTML = invoice.li_due_date;
+    });
+
+    // console.log(invoice);
+  })
+  .catch((error) => {
+    // Handle any errors
+    console.error("Error:", error);
+  });
