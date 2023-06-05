f839d0add26fdf72aadb2dc3fa1220751d7affcd
Aurelio Killian Lexi Verrill <aurelioklv@gmail.com>
2023-06-05 18:46:12 +0700

    feat(home): add name and balance

--- js/home.js
+++ js/home.js
@@ -1,33 +1,53 @@
+const locale = "id-ID";
+const dateOptions = {
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+
+const formatter = new Intl.NumberFormat(locale, {
+  style: "currency",
+  currency: "IDR",
+});
+
+fetch("http://localhost:5000/account")
+  .then((response) => response.json())
+  .then((data) => {
+    data = data[0];
+    const name = data.acc_name;
+    const balance = formatter.format(data.acc_balance);
+
+    const nameElement = document.getElementById("acc_name");
+    const balanceElement = document.getElementById("acc_balance");
+
+    nameElement.innerText = `Halo, ${name}`;
+    balanceElement.innerText = balance;
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+
 fetch("http://localhost:5000/invoices")
   .then((response) => response.json())
-  .then((invoices) => {
-    // Handle the retrieved invoice
-    // Select the elements
+  .then((data) => {
     const container = document.querySelector(".li_container");
-    const dateOptions = {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    };
 
-    const formatter = new Intl.NumberFormat("id-ID", {
-      style: "currency",
-      currency: "IDR",
-    });
+    const paidInvoices = data.filter((invoice) => invoice.li_status === false);
+    const billedInvoices = data.filter((invoice) => invoice.li_status === true);
 
+    console.log(paidInvoices);
+    console.log(billedInvoices);
+
+    const invoices = paidInvoices.concat(billedInvoices);
     invoices.forEach((invoice) => {
-      console.log(invoice);
-      console.log(invoice.li_id);
       const date = new Date(invoice.li_due_date).toLocaleDateString(
-        "id-ID",
+        locale,
         dateOptions
       );
       const money = formatter.format(invoice.li_total_payment);
 
-      console.log(invoice.li_total_payment);
-      console.log(money);
-
       const invoiceElement = document.createElement("div");
+      const hr = document.createElement("hr");
       invoiceElement.id = invoice.li_id;
       invoiceElement.innerHTML = `
         <div class="grid grid-cols-4 gap-5 justify-between text-align-start">
@@ -45,15 +65,23 @@ fetch("http://localhost:5000/invoices")
           </div>
           <div class="m-auto">
             <h2
-              class="text-sm font-semibold text-emerald-600 bg-emerald-50 py-1 px-4 w-auto rounded-full text-center border border-emerald-300"
+              class="text-sm font-semibold py-1 px-4 w-auto rounded-full text-center border ${
+                invoice.li_status
+                  ? "text-emerald-600 bg-emerald-50 border-emerald-300"
+                  : "text-red-600 bg-red-100 border-red-300"
+              }"
             >
-              Sudah dibayar
+              ${invoice.li_status ? "Sudah dibayar" : "Belum dibayar"}
             </h2>
           </div>
           <div class="my-auto text-right">
             <button
-            class=" font-inter bg-slate-100 text-slate-500 font-medium w-auto py-2 px-8 rounded-md disabled:opacity-50"
-            disabled
+            class=" font-inter  font-medium w-auto py-2 px-8 rounded-md ${
+              invoice.li_status
+                ? "bg-slate-100 text-slate-500 disabled:opacity-50"
+                : "bg-orange-100 text-orange-500 hover:bg-orange-200"
+            }"
+            ${invoice.li_status ? "disabled" : ""}
             >
               Bayar
             </button>
@@ -61,16 +89,7 @@ fetch("http://localhost:5000/invoices")
           
         </div>`;
       container.appendChild(invoiceElement);
-      // container.appendChild("<hr />");
-
-      console.log(invoice.li_status);
-      // const idElement = document.querySelector(".li_id");
-      // const totalPaymentElement = document.querySelector(".li_total_payment");
-      // const dueDateElement = document.querySelector(".li_due_date");
-      // // Insert the data into the elements
-      // idElement.innerHTML = invoice.li_id;
-      // totalPaymentElement.innerHTML = invoice.li_total_payment;
-      // dueDateElement.innerHTML = invoice.li_due_date;
+      container.appendChild(hr);
     });
 
     // console.log(invoice);
